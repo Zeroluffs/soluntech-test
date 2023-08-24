73252294777446c5167809bd33333e68d8d72c02
zeroluffs <zeroluffs@gmail.com>
2023-08-24 08:08:43 -0500

    added password to users

--- back/models/accounts.js
+++ back/models/accounts.js
@@ -1,14 +1,22 @@
 // Import required modules and setup the database and ORM
-const { db, Orm } = require('../lib/orm');
-const { Agreement } = require('./aggreement');
+const { db, Orm } = require("../lib/orm");
+const { Agreement } = require("./aggreement");
 
 // Define the Account model with its attributes
-const Account = db.define('Account', {
+const Account = db.define("Account", {
   firstName: {
     type: Orm.STRING,
     allowNull: false,
   },
   lastName: {
+    type: Orm.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: Orm.STRING,
+    allowNull: false,
+  },
+  password: {
     type: Orm.STRING,
     allowNull: false,
   },
@@ -20,15 +28,15 @@ const Account = db.define('Account', {
     type: Orm.DECIMAL(12, 2),
   },
   type: {
-    type: Orm.ENUM('buyer', 'supplier'),
+    type: Orm.ENUM("buyer", "supplier"),
   },
 });
 
 // Set up associations between the Account and Agreement models
-Agreement.belongsTo(Account, { as: 'Supplier' });
-Agreement.belongsTo(Account, { as: 'Buyer' });
-Account.hasMany(Agreement, { as: 'Supplier', foreignKey: 'SupplierId' });
-Account.hasMany(Agreement, { as: 'Buyer', foreignKey: 'BuyerId' });
+Agreement.belongsTo(Account, { as: "Supplier" });
+Agreement.belongsTo(Account, { as: "Buyer" });
+Account.hasMany(Agreement, { as: "Supplier", foreignKey: "SupplierId" });
+Account.hasMany(Agreement, { as: "Buyer", foreignKey: "BuyerId" });
 
 // Export the Account model to be used in other parts of the application
 module.exports = {
