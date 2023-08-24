471472d2f8992e57a59f3f0005024a71a12fa6f3
zeroluffs <zeroluffs@gmail.com>
2023-08-24 14:51:23 -0500

    service for login

--- back/controllers/users.controller.js
+++ back/controllers/users.controller.js
@@ -1,42 +1,22 @@
 const { db, Orm } = require("../lib/orm");
 var jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const { Login, getAllUsers } = require("../services/user.service");
 
 const userCtrl = {};
 
 userCtrl.getUsers = async (req, res) => {
-  const users = await db.query("SELECT * FROM accounts");
+  const users = await getAllUsers();
   res.json(users);
 };
 
 userCtrl.login = async (req, res) => {
   const { username, password } = req.body;
-  if (!(username && password)) {
-    res.status(400).send("All input is required");
-  }
-  console.log(username);
-  const sql = `SELECT * FROM accounts WHERE username = '${username}'`;
-  const results = await db.query(sql);
-  const user = results[0];
-  if (user.length > 0) {
-    const match = await bcrypt.compare(password, user[0].password);
-    if (match) {
-      const token = jwt.sign(
-        {
-          userId: user[0].id,
-          username: user[0].username,
-          profession: user[0].profession,
-          balance: user[0].balance,
-        },
-        "secret",
-        { expiresIn: "2h" }
-      );
-      res.status(200).send({
-        token,
-      });
-    } else {
-      res.status(400).send("Invalid Credentials");
-    }
+  try {
+    const user = await Login(username, password);
+    res.json({ token: user });
+  } catch (err) {
+    res.status(err.code).json({ message: err.message });
   }
 };
 
---
+++ back/services/user.service.js
@@ -0,0 +1,40 @@
+const { db, Orm } = require("../lib/orm");
+var jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { status } = require("express/lib/response");
+const CustomError = require("../classUtils/CustomError");
+
+const userService = {};
+
+userService.getAllUsers = async () => {
+  return await db.query("SELECT * FROM accounts");
+};
+
+userService.Login = async (username, password) => {
+  if (!(username && password)) {
+    // return status(400).send("All input is required");
+    throw new CustomError("All input is required", 400);
+  }
+  const sql = `SELECT * FROM accounts WHERE username = '${username}'`;
+  const results = await db.query(sql);
+  const user = results[0];
+  if (user.length > 0) {
+    const match = await bcrypt.compare(password, user[0].password);
+    if (match) {
+      return jwt.sign(
+        {
+          userId: user[0].id,
+          username: user[0].username,
+          profession: user[0].profession,
+          balance: user[0].balance,
+        },
+        "secret",
+        { expiresIn: "2h" },
+      );
+    } else {
+      throw new CustomError("Invalid Credentials", 400);
+    }
+  }
+};
+
+module.exports = userService;
