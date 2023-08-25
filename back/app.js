c97de813da3eb26d8426e1bb51edc348221ebc9a
zeroluffs <zeroluffs@gmail.com>
2023-08-24 19:48:08 -0500

    added cors

--- back/app.js
+++ back/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var agreementRouter = require("./routes/agreement");
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({ origin: "*" }));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
