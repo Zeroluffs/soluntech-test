c3474a671a9a8103adc17e79b3300c36477cdf97
zeroluffs <zeroluffs@gmail.com>
2023-08-25 13:43:41 -0500

    fixed unpaid submissions

--- back/services/submissions.service.js
+++ back/services/submissions.service.js
@@ -4,7 +4,7 @@ const submissionsService = {};
 
 submissionsService.getUnpaidSubmissionsPerUser = async (userId) => {
   const [submissions] = await db.query(
-    `SELECT s.* FROM submissions s INNER JOIN agreements a ON s.AgreementId = a.id WHERE a.status IN ('in_progress') AND (a.BuyerId = ${userId} OR a.SupplierId = ${userId})`,
+    `SELECT s.* FROM submissions s INNER JOIN agreements a ON s.AgreementId = a.id WHERE a.status IN ('in_progress') AND (a.BuyerId = ${userId} OR a.SupplierId = ${userId}) AND s.paid = 0`
   );
   if (submissions.length === 0) {
     throw new CustomError("No submissions", 404);
@@ -14,38 +14,38 @@ submissionsService.getUnpaidSubmissionsPerUser = async (userId) => {
 
 submissionsService.payUserSubmission = async (id, price, userId) => {
   const [buyer] = await db.query(
-    `SELECT * FROM accounts WHERE id = ${userId} AND  type = 'buyer'`,
+    `SELECT * FROM accounts WHERE id = ${userId} AND  type = 'buyer'`
   );
 
   const [agreement] = await db.query(
-    `SELECT * FROM agreements INNER JOIN submissions ON submissions.AgreementId = agreements.id WHERE submissions.id = ${id} AND (agreements.BuyerId = ${userId}) AND (agreements.status = 'in_progress' OR agreements.status = 'new')`,
+    `SELECT * FROM agreements INNER JOIN submissions ON submissions.AgreementId = agreements.id WHERE submissions.id = ${id} AND (agreements.BuyerId = ${userId}) AND (agreements.status = 'in_progress' OR agreements.status = 'new')`
   );
   const [submission] = await db.query(
-    `SELECT * FROM submissions WHERE id = ${id} AND paid = 0`,
+    `SELECT * FROM submissions WHERE id = ${id} AND paid = 0`
   );
   if (submission.length === 0) {
     throw new CustomError("Submission Already Paid", 404);
   }
   const suppliedId = agreement[0].SupplierId;
   const [supplier] = await db.query(
-    `SELECT * FROM accounts WHERE id = ${suppliedId} AND  type = 'supplier'`,
+    `SELECT * FROM accounts WHERE id = ${suppliedId} AND  type = 'supplier'`
   );
   if (buyer[0].balance < price) {
     throw new CustomError("Insufficient Funds", 400);
   }
   buyer[0].balance = buyer[0].balance - price;
   await db.query(
-    `UPDATE accounts SET balance = ${buyer[0].balance} WHERE id = ${userId}`,
+    `UPDATE accounts SET balance = ${buyer[0].balance} WHERE id = ${userId}`
   );
   supplier[0].balance = supplier[0].balance + price;
   await db.query(
-    `UPDATE accounts SET balance = ${supplier[0].balance} WHERE id = ${suppliedId}`,
+    `UPDATE accounts SET balance = ${supplier[0].balance} WHERE id = ${suppliedId}`
   );
   agreement[0].status = "terminated";
   submission[0].paid = 1;
   await db.query(`UPDATE submissions SET paid = 1 WHERE id = ${id}`);
   await db.query(
-    `UPDATE agreements SET status = 'terminated' WHERE id = ${agreement[0].id}`,
+    `UPDATE agreements SET status = 'terminated' WHERE id = ${agreement[0].id}`
   );
 };
 
