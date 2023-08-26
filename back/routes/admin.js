29a9b01eb46aedf62d4a55ff204a21c0335a3538
zeroluffs <zeroluffs@gmail.com>
2023-08-26 10:04:02 -0500

    verify token middleware

--- back/routes/admin.js
+++ back/routes/admin.js
@@ -1,10 +1,12 @@
 var express = require("express");
 const adminCtrl = require("../controllers/admin.controller");
+const verifyToken = require("../middlewares/jsonwebtoknen");
 var router = express.Router();
 
 /* GET users listing. */
 router.get(
   "/best-supplier-profession/:start/:end",
+  verifyToken,
   adminCtrl.getBestProfessions
 );
 
