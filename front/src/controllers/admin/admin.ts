bc16e467c26c97c80245687c5a892a6d36c744c7
zeroluffs <zeroluffs@gmail.com>
2023-08-26 10:03:38 -0500

    sends header on token

--- front/src/controllers/admin/admin.ts
+++ front/src/controllers/admin/admin.ts
@@ -1,15 +1,14 @@
 import { BASE_URL } from "@/controllers/consts";
 
 export async function getUsers(from: string, to: string) {
-  const start = from;
-  const end = to;
+  const token = JSON.parse(localStorage.getItem("token") || "null");
   const response = await fetch(
-    `${BASE_URL}/admin/best-supplier-profession/${start}/${end}`,
+    `${BASE_URL}/admin/best-supplier-profession/${from}/${to}`,
     {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        // "x-access-token": token,
+        "x-access-token": token,
       },
     },
   );
