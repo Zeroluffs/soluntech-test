57a25868b5d77a78b35f10bfa10007c50886b217
zeroluffs <zeroluffs@gmail.com>
2023-08-26 10:30:02 -0500

    shows message

--- front/src/components/DatePickerWithRange.tsx
+++ front/src/components/DatePickerWithRange.tsx
@@ -23,62 +23,75 @@ export function DatePickerWithRange({
     from: new Date(2023, 7, 20),
     to: addDays(new Date(2023, 7, 20), 20),
   });
+  const [message, setMessage] = React.useState<string>("");
   async function searchUsers() {
-    console.log(date);
     if (date!.from !== null && date!.to !== null) {
       // @ts-ignore
       const from = date.from.toDateString();
       const to = date!!.to!!.toDateString();
       const { fromDate, endDate } = formatDate(from, to);
-      await getUsers(fromDate, endDate);
+      const objectDate = await getUsers(fromDate, endDate);
+      if (objectDate.message) {
+        setMessage(objectDate.message);
+      } else {
+        const str = `The best profession was ${
+          objectDate.best_buyer_profession
+        } with a total earned of $${objectDate.total_earned.toFixed(
+          2,
+        )} for the given time period`;
+        setMessage(str);
+      }
     }
   }
   console.log(date);
   return (
-    <div className={cn("grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground",
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
+    <div className={"flex flex-col gap-6"}>
+      <div className={cn("grid gap-2", className)}>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              id="date"
+              variant={"outline"}
+              className={cn(
+                "w-[300px] justify-start text-left font-normal",
+                !date && "text-muted-foreground",
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date?.from ? (
+                date.to ? (
+                  <>
+                    {format(date.from, "LLL dd, y")} -{" "}
+                    {format(date.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(date.from, "LLL dd, y")
+                )
               ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
-      <Button
-        disabled={date === null}
-        onClick={searchUsers}
-        className="w-[144px] mt-2"
-      >
-        Search
-      </Button>
+                <span>Pick a date</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              initialFocus
+              mode="range"
+              defaultMonth={date?.from}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={2}
+            />
+          </PopoverContent>
+        </Popover>
+        <Button
+          disabled={date?.from === undefined || date?.to === undefined}
+          onClick={searchUsers}
+          className="w-[144px] mt-2"
+        >
+          Search
+        </Button>
+      </div>
+      <p className={"text-lg font-semibold"}>{message}</p>
     </div>
   );
 }
--- front/src/controllers/admin/admin.ts
+++ front/src/controllers/admin/admin.ts
@@ -2,14 +2,20 @@ import { BASE_URL } from "@/controllers/consts";
 
 export async function getUsers(from: string, to: string) {
   const token = JSON.parse(localStorage.getItem("token") || "null");
-  const response = await fetch(
-    `${BASE_URL}/admin/best-supplier-profession/${from}/${to}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-access-token": token,
+  try {
+    const response = await fetch(
+      `${BASE_URL}/admin/best-supplier-profession/${from}/${to}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": token,
+        },
       },
-    },
-  );
+    );
+    return response.json();
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
