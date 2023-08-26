67c0ab33d20c8f7b9a42358184533b2c991bd2a6
zeroluffs <zeroluffs@gmail.com>
2023-08-25 19:24:16 -0500

    refreshes otken

--- back/routes/token.js
+++ back/routes/token.js
@@ -1,20 +1,39 @@
 var express = require("express");
 var router = express.Router();
 const jwt = require("jsonwebtoken");
+const { db } = require("../lib/orm");
 
-router.get("/verify", function (req, res) {
+router.get("/verify", async function (req, res) {
   const token = req.headers["x-access-token"];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, "secret");
-    res.status(200).send({
+    jwt.verify(token, "secret");
+    const getuser = jwt.decode(token);
+    const userId = getuser.userId;
+
+    const [user] = await db.query(
+      `SELECT * FROM accounts WHERE id = ${userId}`
+    );
+
+    return res.status(200).send({
       isValid: true,
+      newToken: jwt.sign(
+        {
+          userId: user[0].id,
+          username: user[0].username,
+          profession: user[0].profession,
+          balance: user[0].balance,
+          role: user[0].type,
+        },
+        "secret",
+        { expiresIn: "2h" }
+      ),
     });
   } catch (err) {
-    return res.status(401).send({
+    res.status(401).send({
       isValid: false,
     });
   }
