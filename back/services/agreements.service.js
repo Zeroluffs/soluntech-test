9ced87f815573c1bb3d7eec47d9968138bbb7533
zeroluffs <zeroluffs@gmail.com>
2023-08-26 16:09:27 -0500

    agreements orm

--- back/services/agreements.service.js
+++ back/services/agreements.service.js
@@ -1,5 +1,6 @@
-const { db } = require("../lib/orm");
+const { db, Orm } = require("../lib/orm");
 const CustomError = require("../classUtils/CustomError");
+const { Agreement } = require("../models/agreement");
 const agreementsService = {};
 
 agreementsService.getAgreementById = async (agreementId, userId) => {
@@ -13,14 +14,17 @@ agreementsService.getAgreementById = async (agreementId, userId) => {
 };
 
 agreementsService.getUserAgreements = async (userId) => {
-  const [agreements] = await db.query(
-    `SELECT * FROM agreements WHERE status IN ('new', 'in_progress') AND (BuyerId = ${userId} OR SupplierId = ${userId})
-        `
-  );
+  const agreements = await Agreement.findAll({
+    where: {
+      status: ["new", "in_progress"],
+      [Orm.Op.or]: [{ BuyerId: userId }, { SupplierId: userId }],
+    },
+  });
+
   if (agreements.length === 0) {
     throw new CustomError("No Agreements", 404);
   }
+
   return agreements;
 };
-
 module.exports = agreementsService;
