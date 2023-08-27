91684d7e3cc6c191debca996bc190ef9386fd787
zeroluffs <zeroluffs@gmail.com>
2023-08-26 19:32:38 -0500

    fixing server

--- back/app.js
+++ back/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var http = require("http");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
@@ -10,6 +11,7 @@ var submissionRouter = require("./routes/submission");
 const balanceRouter = require("./routes/balance");
 const tokenRouter = require("./routes/token");
 const adminRouter = require("./routes/admin");
+
 var app = express();
 
 app.use(logger("dev"));
@@ -26,13 +28,25 @@ app.use("/submissions", submissionRouter);
 app.use("/balances", balanceRouter);
 app.use("/token", tokenRouter);
 app.use("/admin", adminRouter);
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+
 const host = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || 3001;
-// module.exports = app;
-app.listen(port, () => {
-  console.log("Server is Working", port);
-});
+let port = process.env.PORT || 3001;
+
+const server = http.createServer(app);
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  port++;
+  server.listen(port);
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Listening on " + bind);
+}
